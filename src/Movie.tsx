7af3aeb5f663af776cf0f5aa807303f8f4ce7fb1
Thomas Wilson <tommyhwilson3@gmail.com>
2023-09-06 13:44:12 -0500

    Started interfaces file and added first interface, started work on Movie component

--- src/Movie.tsx
+++ src/Movie.tsx
@@ -1,3 +1,23 @@
-export default function Movie(){
+import { Typography } from "@mui/material";
+import { Movie } from "./interfaces";
 
+interface MovieProps {
+    movie: Movie
 }
+
+export default function Movie({ movie }: MovieProps){
+
+    return(
+        <div className="Movie">
+            <Typography variant="h6">{movie.title}</Typography>
+            <img
+                src={movie.image}
+                alt={`${movie.title} poster`}
+                height="300"
+                width="200"
+            />
+        </div>
+    )
+}
+
+// ["title", "poster_path", "release_date", "overview"]
---
+++ src/interfaces.tsx
@@ -0,0 +1,12 @@
+
+
+export interface Movie{
+    id: number,
+    title: string,
+    image?: string,
+    release_date?: string,
+    runtime?: string,
+    genre?: string,
+    bio?: string
+    is_watched: boolean
+}
