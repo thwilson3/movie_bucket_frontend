bc991037255de4bfd56b1c51eff2034291303183
Thomas Wilson <tommyhwilson3@gmail.com>
2023-09-11 16:24:32 -0500

    Added a few more interfaces to define the data coming from the db

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -7,6 +7,27 @@ export interface Movie{
     release_date?: string,
     runtime?: string,
     genre?: string,
-    bio?: string
-    is_watched: boolean
+    bio?: string,
+    is_watched: boolean,
+}
+
+export interface Bucket{
+    id: number,
+    bucket_name: string,
+    genre?: string,
+    description?: string,
+}
+
+export interface User{
+    id: number,
+    username: string,
+    email: string,
+    password: string,
+}
+
+export interface BucketLink{
+    id: number,
+    bucket_id: number,
+    invite_code: string,
+    expiration_date: string,
 }
