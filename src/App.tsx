a7ee6e3fadfa5d9548bfeade30351ddbf3321036
Thomas Wilson <tommyhwilson3@gmail.com>
2023-09-14 15:27:12 -0500

    Created a basic navigation bar, which app renders

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navigation from './Navigation';
 
 export default function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <Navigation />
+      {/* <Switch> */}
+        {/* <Route path="/" exact component={Homepage} />
+        <Route path="/my-buckets" component={BucketList} />
+        <Route path="/account" component={Account} /> */}
+      {/* </Switch> */}
+    </Router>
   )
 }
--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,3 +1,28 @@
+import { AppBar, Toolbar, Typography, Button, Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+/** Navigation bar
+ *
+ *
+ *  App -> Navigation
+ */
 export default function Navigation(){
+    return (
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6" component={Link} to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+              Movie Bucket
+            </Typography>
+            <Stack direction="row" spacing={2} sx={{marginLeft: 'auto'}}>
+                <Button color="inherit" component={Link} to="/about">
+                My Buckets
+                </Button>
+                <Button color="inherit" component={Link} to="/contact">
+                Account
+                </Button>
+            </Stack>
+          </Toolbar>
+        </AppBar>
+      );
 
 }
