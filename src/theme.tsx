5f2620708f3693f9a37bd28b3cf5e26396e9cc13
Thomas Wilson <tommyhwilson3@gmail.com>
2023-09-14 15:12:10 -0500

    Created a custom theme for MUI use

---
+++ src/theme.tsx
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#3B3840',
+      },
+      secondary: {
+        main: '#BF8E8E',
+      },
+    },
+    typography: {
+      fontFamily: 'Kanit',
+    },
+  });
