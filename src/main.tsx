d06e3036d3f897f3801d7605b71c1c86e1848c2b
Thomas Wilson <tommyhwilson3@gmail.com>
2023-09-14 15:12:49 -0500

    Wrapped App with ThemeProvider component for MUI use

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from '@mui/material'
+import { CssBaseline } from '@mui/material'
 import App from './App.tsx'
+import { theme } from './theme.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
+    </ThemeProvider>
 )
