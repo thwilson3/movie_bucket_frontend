cd3b105e2db5eda38779b14a47720fe477693ed3
Thomas Wilson <tommyhwilson3@gmail.com>
2023-09-15 17:39:07 -0500

    Started display list for movies in bucket

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,4 +1,15 @@
+import { FixedSizeList } from 'react-window'
 
 export default function Homepage(){
-
+return (
+    <FixedSizeList
+    height={400}
+    width={360}
+    itemSize={46}
+    itemCount={200}
+    overscanCount={5}
+    >
+    {renderRow}
+    </FixedSizeList>
+)
 }
