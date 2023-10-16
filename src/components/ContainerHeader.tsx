bd2d38c4c89cf2a1846e49ca66cfeff3aadaeed4
Thomas Wilson <tommyhwilson3@gmail.com>
2023-10-16 17:39:52 -0500

    Add new components for better UI

---
+++ src/components/ContainerHeader.tsx
@@ -0,0 +1,12 @@
+export default function ContainerHeader() {
+  return (
+    <>
+        <div className="flex py-2 md:py-2 font-bold bg-cyan-100 justify-center outline outline-2 lg:py-8 top-0 left-0 absolute w-full">
+            welcome
+        </div>
+        <div className="bg-primary font-bold outline outline-2 absolute right-5 top-2 px-2 cursor-pointer">
+            login
+        </div>
+    </>
+  );
+}
---
+++ src/components/LandingOptions.tsx
@@ -0,0 +1,10 @@
+export default function LandingOptions() {
+  return (
+    <div
+            className="flex cursor-pointer text-black justify-center font-bold outline outline-2 w-full max-w-sm py-6 bg-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] md:shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[8px] hover:translate-y-[8px] hover:shadow-none"
+
+          >
+            Party Bucket
+          </div>
+  );
+}
