92c6b2cfcbbca6aae1e73e9228661b5531fddff2
Thomas Wilson <tommyhwilson3@gmail.com>
2023-10-19 17:50:54 -0500

    Update variable name for type import

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,8 +1,8 @@
 import { Typography } from "@mui/material";
-import { Movie } from "../interfaces";
+import { MovieType } from "../interfaces";
 
 interface MovieProps {
-    movie: Movie
+    movie: MovieType
 }
 
 /** Renders a single movie component
