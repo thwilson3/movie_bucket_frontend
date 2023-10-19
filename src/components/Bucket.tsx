2952f5809383e337319f523c46f425ece62fe67a
Thomas Wilson <tommyhwilson3@gmail.com>
2023-10-19 17:50:01 -0500

    Migrate common tailwind style to higher component

--- src/components/Bucket.tsx
+++ src/components/Bucket.tsx
@@ -15,7 +15,7 @@ type BucketProps = {
 export default function Bucket({bucket, idx}: BucketProps){
     return (
         <div
-            className="flex cursor-pointer text-black justify-center font-bold outline outline-2 w-full max-w-sm py-6 bg-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] md:shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[8px] hover:translate-y-[8px] hover:shadow-none"
+            className="flex cursor-pointer text-black justify-center font-bold outline outline-2 w-60 md:w-80 lg:w-96 max-w-sm py-6 bg-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] md:shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[8px] hover:translate-y-[8px] hover:shadow-none"
             key={idx}
           >
             {bucket.bucket_name}
