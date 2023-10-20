4bbf67360a267dcae3fe1308723ccba24d32f661
Thomas Wilson <tommyhwilson3@gmail.com>
2023-10-20 16:31:08 -0500

    Add functional login form

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function LoginForm({ login }) {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [formErrors, setFormErrors] = useState([]);
+
+  console.debug(
+    "LoginForm",
+    "login=",
+    typeof login,
+    "formData=",
+    formData,
+    "formErrors",
+    formErrors
+  );
+
+  /** Handle form submit:
+   *
+   * Calls login func prop and, if not successful, sets errors.
+   */
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+    try {
+      await login(formData);
+      navigate("/");
+    } catch (err) {
+      setFormErrors(err);
+    }
+  }
+
+  /** Update form data field */
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setFormData((l) => ({ ...l, [name]: value }));
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="border-2 flex bg-white flex-row border-black font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
+          <label className="bg-black text-white p-3">username</label>
+          <input
+            name="username"
+            className="px-1"
+            value={formData.username}
+            onChange={handleChange}
+            autoComplete="username"
+            required
+          />
+        </div>
+        <div className="border-2 flex bg-white flex-row border-black mt-8 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
+          <label className="bg-black text-white p-3">password</label>
+          <input
+            name="password"
+            className="px-1"
+            value={formData.password}
+            onChange={handleChange}
+            autoComplete="current-password"
+            required
+          />
+        </div>
+
+        {/* {formErrors.length ? (
+          <Alert type="danger" messages={formErrors} />
+        ) : null} */}
+
+        <div className="flex justify-end">
+          <button
+            className="bg-primary mt-4 font-bold w-20 p-2 outline outline-2 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]"
+            onClick={handleSubmit}
+          >
+            Enter
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
