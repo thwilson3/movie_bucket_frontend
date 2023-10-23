f4e605de4f87d04c4e0a26a76178c3d7a0dd9e20
Thomas Wilson <tommyhwilson3@gmail.com>
2023-10-23 17:27:47 -0500

    Fix bug in input background

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -38,15 +38,15 @@ export default function LoginForm({ login }) {
     const { name, value } = evt.target;
     setFormData((l) => ({ ...l, [name]: value }));
   }
-    //FIXME: Something buggy is happening with the username input bg-color, I suspect it's being overridden by something higher up
+
   return (
-    <>
+    <div className="bg-inherit opacity-100">
       <form onSubmit={handleSubmit}>
         <div className="border-2 flex flex-row border-black font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
-          <label className="bg-black outline outline-2 text-black p-3">username</label>
+          <label className="bg-black outline outline-black outline-2 text-white p-3">username</label>
           <input
             name="username"
-            className="px-1 bg-red-600"
+            className="px-1 bg-white"
             value={formData.username}
             onChange={handleChange}
             autoComplete="username"
@@ -54,10 +54,10 @@ export default function LoginForm({ login }) {
           />
         </div>
         <div className="border-2 flex flex-row border-black mt-8 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
-          <label className="bg-black outline outline-2 text-black p-3">password</label>
+          <label className="bg-black outline outline-black outline-2 text-white p-3">password</label>
           <input
             name="password"
-            className="px-1 bg-red-600"
+            className="px-1 bg-white"
             value={formData.password}
             onChange={handleChange}
             autoComplete="current-password"
@@ -78,6 +78,6 @@ export default function LoginForm({ login }) {
           </button>
         </div>
       </form>
-    </>
+    </div>
   );
 }
