4ae7cc6ee32b8ab3d09899614ff56b0f3aaa8487
Thomas Wilson <tommyhwilson3@gmail.com>
2023-10-31 15:20:33 -0500

    Typed props

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
+import { MovieType } from "../interfaces";
+
 import Movie from "./Movie";
 
-export default function MovieList({ movies }) {
+export default function MovieList({ movies }: {movies: MovieType[]}) {
   return (
     <div className="flex flex-col gap-6">
       {movies.map((movie) => (
