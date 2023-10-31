e30c2535e289c0a09dc24494781a56fd7310c643
Thomas Wilson <tommyhwilson3@gmail.com>
2023-10-31 15:01:01 -0500

    Add options to make component more dynamic

--- src/components/ContainerHeader.tsx
+++ src/components/ContainerHeader.tsx
@@ -2,28 +2,28 @@ import { useContext } from "react";
 import { UserContext } from "../UserContext";
 import { UserContextType } from "../interfaces";
 import { Link } from "react-router-dom";
+import { HeaderOptions } from "../interfaces";
 
-export default function ContainerHeader({ logout }) {
+export default function ContainerHeader({ options }: {options?: HeaderOptions}) {
   const { currentUser } = useContext(UserContext) as UserContextType;
+
+  function handleClick() {
+    if (options?.function) {
+      options?.function();
+    }
+  }
   return (
     <>
       <div className="flex py-2 md:py-2 font-bold bg-secondary justify-center outline outline-2 lg:py-8 top-0 left-0 absolute w-full">
-        {currentUser?.username ? currentUser.username : 'welcome'}
+        {options?.title ? options.title : "welcome"}
       </div>
-      {currentUser === null ? (
-        <Link to={"login"}>
+      {options !== undefined ? (
+        <Link to={options?.path} onClick={handleClick}>
           <div className="bg-primary font-bold outline outline-2 absolute right-5 top-2 px-2 cursor-pointer">
-            login
+            {options?.text}
           </div>
         </Link>
-      ) : (
-        <Link to={"/"}>
-          <div className="bg-primary font-bold outline outline-2 absolute right-5 top-2 px-2 cursor-pointer"
-          onClick={logout}>
-            logout
-          </div>
-        </Link>
-      )}
+      ) : null}
     </>
   );
 }
