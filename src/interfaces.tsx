ce832208f8256c3509ae39d8eeaaa8562589af99
Thomas Wilson <tommyhwilson3@gmail.com>
2023-11-01 13:57:23 -0500

    Add types for functions

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -1,45 +1,46 @@
-
-
-export interface MovieType{
-    id: number,
-    title: string,
-    image?: string,
-    release_date?: string,
-    runtime?: string,
-    genre?: string,
-    bio?: string,
-    is_watched: boolean,
+export interface MovieType {
+  id: number;
+  title: string;
+  image?: string;
+  release_date?: string;
+  runtime?: string;
+  genre?: string;
+  bio?: string;
+  is_watched: boolean;
 }
 
-export interface BucketType{
-    id: number,
-    bucket_name: string,
-    genre?: string,
-    description?: string,
+export interface BucketType {
+  id: number;
+  bucket_name: string;
+  genre?: string;
+  description?: string;
 }
 
-export interface UserType{
-    id: number,
-    username: string,
-    email: string,
-    password: string,
+export interface UserType {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
 }
 
-export interface BucketLinkType{
-    id: number,
-    bucket_id: number,
-    invite_code: string,
-    expiration_date: string,
+export interface BucketLinkType {
+  id: number;
+  bucket_id: number;
+  invite_code: string;
+  expiration_date: string;
 }
 
 export type UserContextType = {
-    currentUser?: UserType | null,
-    setCurrentUser: (user: UserType | null) => void
-}
+  currentUser?: UserType | null;
+  setCurrentUser: (user: UserType | null) => void;
+};
 
 export type HeaderOptions = {
-    title?: string;
-    text: string;
-    function?: () => void;
-    path: string;
-  };
+  title?: string;
+  text: string;
+  function?: Function;
+  path: string;
+};
+
+export type LoginFunction = (username: string, password: string) => void;
+export type LogoutFunction = () => void;
