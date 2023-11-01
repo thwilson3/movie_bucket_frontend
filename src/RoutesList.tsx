867eeca679b4cabc270f5bed577ff0659a4825c2
Thomas Wilson <tommyhwilson3@gmail.com>
2023-11-01 13:45:58 -0500

    Add route for bucket form

--- src/RoutesList.tsx
+++ src/RoutesList.tsx
@@ -8,6 +8,8 @@ import SignUpForm from "./components/SignUpForm";
 import LoginContainer from "./components/LogInContainer";
 import BucketsContainer from "./components/BucketsContainer";
 import MoviesContainer from "./components/MoviesContainer";
+import BucketForm from "./components/BucketForm";
+import AddBucketContainer from "./components/AddBucketContainer";
 
 export default function RoutesList({ signup, login, logout }) {
     const { currentUser } = useContext(UserContext) as UserContextType;
@@ -18,6 +20,7 @@ export default function RoutesList({ signup, login, logout }) {
         <Route path="/login" element={<LoginContainer login={login} logout={logout}/>} />
         <Route path="/signup" element={<SignUpForm signup={signup} />} />
         <Route path="/buckets" element={currentUser !== null ? <BucketsContainer /> : <Navigate to={"/"} />} />
+        <Route path="/buckets/add" element={currentUser !== null ? <AddBucketContainer /> : <Navigate to={"/"} />}/>
         <Route path="/buckets/:id" element={currentUser !== null ? <MoviesContainer /> : <Navigate to={"/"} />}/>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
