6fb89b869247853dbae31e9a3dfc7b1615cffc91
Thomas Wilson <tommyhwilson3@gmail.com>
2023-11-01 15:25:06 -0500

    Adjust UI

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,8 +1,10 @@
 import { MovieType } from "../interfaces";
 
 interface MovieProps {
-    movie: MovieType
+  movie: MovieType;
 }
+
+const NO_IMAGE_FOUND = "../public/No image.png";
 
 /** Renders a single movie component
  *
@@ -12,17 +14,19 @@ interface MovieProps {
  *
  *  MovieList -> Movie
  */
-export default function Movie({ movie }: MovieProps){
-
-    return(
-        <div className="Movie" key={movie.id}>
-            {movie.title}
-            <img
-                src={movie.image}
-                alt={`${movie.title} poster`}
-                height="300"
-                width="200"
-            />
-        </div>
-    )
+export default function Movie({ movie }: MovieProps) {
+  return (
+    <div
+      className="flex flex-col cursor-pointer text-white justify-center items-center font-bold outline outline-black outline-2 w-60 md:w-80 lg:w-96 max-w-sm py-6 bg-secondary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] md:shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[8px] hover:translate-y-[8px] hover:shadow-none"
+      key={movie.id}
+    >
+      {movie.title}
+      <img
+        src={movie.image ? movie.image : NO_IMAGE_FOUND}
+        alt={`${movie.title} poster`}
+        height="300"
+        width="200"
+      />
+    </div>
+  );
 }
