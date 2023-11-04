998b856692628591dfbb3d68a868d1381fb9aeea
Thomas Wilson <tommyhwilson3@gmail.com>
2023-11-03 19:55:56 -0500

    Adding search functionality

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,23 +1,47 @@
 import { useState } from "react";
+import MovieBucketAPI from "../api";
 
-export default function SearchBar({ placeholder }: { placeholder: string }) {
-  const [value, setValue] = useState("");
+export default function SearchBar({ placeholder, getResults }: { placeholder: string, getResults: Function }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  // const [movies, setMovies] = useState([])
+  console.log("searchTerm", searchTerm);
+
+
+  // async function getResults(searchTerm){
+  //   console.log("searchTerm in search bar", searchTerm);
+
+  //   try{
+  //     const movies = await MovieBucketAPI.getSearchResults(searchTerm)
+  //     console.log("movies in search bar", movies);
+  //     setMovies(movies)
+
+  //   } catch(err){
+  //     throw new Error(JSON.stringify(err))
+  //   }
+  // }
+
+  function handleSubmit(evt){
+    evt.preventDefault()
+    getResults(searchTerm)
+  }
+
 
   return (
-    <div className="border-2 flex bg-white flex-row border-black my-6 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
+  <form className="border-2 flex bg-white flex-row border-black my-6 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]"
+  onSubmit={handleSubmit}>
       <input
         className="w-full px-2"
         type="text"
         placeholder={placeholder}
-        value={value}
+        value={searchTerm}
         onChange={(e) => {
-          setValue(e.target.value);
+          setSearchTerm(e.target.value);
         }}
         aria-label={placeholder}
       />
-      <div className="cursor-pointer p-3 text-black justify-center font-bold outline outline-2 bg-primary">
+      <button className="cursor-pointer p-3 text-black justify-center font-bold outline outline-2 bg-primary">
         Search
-      </div>
-    </div>
+      </button>
+    </form>
   );
 }
