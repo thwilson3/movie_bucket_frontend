d3739365dc3653fca322bc3124155ad13f28db39
Thomas Wilson <tommyhwilson3@gmail.com>
2023-11-06 16:36:46 -0600

    Adjust UI

--- src/components/AnonOptions.tsx
+++ src/components/AnonOptions.tsx
@@ -4,12 +4,12 @@ export default function AnonOptions() {
   return (
     <div className="flex flex-col gap-6">
       <Link to={"login"}>
-        <div className="flex cursor-pointer text-black justify-center font-bold outline outline-black outline-2 w-60 md:w-80 lg:w-96 max-w-sm py-6 bg-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] md:shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[8px] hover:translate-y-[8px] hover:shadow-none">
+        <div className="flex cursor-pointer text-black justify-center font-bold outline outline-black outline-2 w-60 md:w-screen lg:w-screen max-w-sm py-6 bg-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] md:shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[8px] hover:translate-y-[8px] hover:shadow-none">
           login
         </div>
       </Link>
       <Link to={"signup"}>
-        <div className="flex cursor-pointer text-black justify-center font-bold outline outline-black outline-2 w-60 md:w-80 lg:w-96 max-w-sm py-6 bg-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] md:shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[8px] hover:translate-y-[8px] hover:shadow-none">
+        <div className="flex cursor-pointer text-black justify-center font-bold outline outline-black outline-2 w-60 md:w-screen lg:w-screen max-w-sm py-6 bg-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] md:shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[8px] hover:translate-y-[8px] hover:shadow-none">
           sign up
         </div>
       </Link>
--- src/components/LandingOptions.tsx
+++ src/components/LandingOptions.tsx
@@ -4,7 +4,7 @@ export default function LandingOptions() {
   return (
     <div className="flex flex-col gap-6">
     <Link to={"buckets"}>
-      <div className="flex cursor-pointer text-black justify-center font-bold outline outline-black outline-2 w-60 md:w-80 lg:w-96 max-w-sm py-6 bg-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] md:shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[8px] hover:translate-y-[8px] hover:shadow-none">
+      <div className="flex cursor-pointer text-black justify-center font-bold outline outline-black outline-2 w-60 md:w-screen lg:w-screen max-w-md py-6 bg-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] md:shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[8px] hover:translate-y-[8px] hover:shadow-none">
         my buckets
       </div>
     </Link>
