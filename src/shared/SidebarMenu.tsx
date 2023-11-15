bb1b9bd9879b50a6128406b794137ab1240fcf93
Thomas Wilson <tommyhwilson3@gmail.com>
2023-11-15 17:02:08 -0600

    Add useState and useEffect to achieve animation effect

--- src/shared/SidebarMenu.tsx
+++ src/shared/SidebarMenu.tsx
@@ -1,9 +1,22 @@
 import { Link } from "react-router-dom";
 import { OptionButtonType } from "../types";
+import { useEffect, useState } from "react";
 
-export default function SidebarMenu({ buttons }: {buttons: OptionButtonType[]}) {
+export default function SidebarMenu({
+  buttons,
+}: {
+  buttons: OptionButtonType[];
+}) {
+  const [marginLeft, setMarginLeft] = useState(-6);
+
+  useEffect(() => {
+    setMarginLeft(1.5);
+  }, []);
   return (
-    <div className="absolute top-11 left-full ml-6 outline outline-2 outline-black bg-primary drop-shadow-lg rounded-r-md w-36 shadow-[3px_3px_0px_3px_rgba(0,0,0,1)]">
+    <div
+      className="absolute top-11 left-full transition-all ease-in-out duration-300 -z-10 outline outline-2 outline-black bg-primary drop-shadow-2xl rounded-r-md rounded-l-md w-36 shadow-[3px_3px_0px_3px_rgba(0,0,0,1)]"
+      style={{ marginLeft: `${marginLeft}rem` }}
+    >
       {buttons.map((button) =>
         button.type === "Link" ? (
           <>
