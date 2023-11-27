8940be5895a4ae859cac04ebdece8d7b0b36743e
Thomas Wilson <tommyhwilson3@gmail.com>
2023-11-27 15:46:09 -0600

    add animated loading spinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,7 +1,16 @@
-export default function LoadingSpinner(){
-    return(
-        <div>
-            loading...
-        </div>
-    )
+import { square } from "ldrs";
+
+square.register();
+
+export default function LoadingSpinner() {
+  return (
+    <l-square
+      size="35"
+      stroke="5"
+      stroke-length="0.25"
+      bg-opacity="0.1"
+      speed="1.2"
+      color="black"
+    ></l-square>
+  );
 }
