b8593578d84059327b8e0beeeb3ad84491744802
Thomas Wilson <tommyhwilson3@gmail.com>
2024-05-10 15:19:20 -0500

    update color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary:'#fff129',
+        primary:'#ffa939',
         secondary: '#4a4a4a',
-        accent: '#e5d1ff',
-        secondAccent: '#a79cb6',
-        background: '#fdf1df'
+        accent: '#d1eaff',
+        secondAccent: '#ddc6ae',
+        background: '#e4ebf9'
       },
-      // fontFamily: {
-      //   mono: ['var(--font-SF-Mono)']
-      // },
       dropShadow: {
         primaryGlow: "0 0 3px #ff9d42",
         whiteGlow: "0 0 3px #b5c0b7",
