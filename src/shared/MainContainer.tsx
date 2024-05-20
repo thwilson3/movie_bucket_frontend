de3fabdf377b6f2deda386761c8d28f4e9958f0d
Thomas Wilson <tommyhwilson3@gmail.com>
2024-05-20 16:19:42 -0500

    clean up

--- src/shared/MainContainer.tsx
+++ src/shared/MainContainer.tsx
@@ -33,13 +33,13 @@ export default function MainContainer({
     <>
       <div className="flex relative items-center flex-col z-10 justify-center mt-16 bg-background">
         <div className="relative">
-          <div className="flex flex-col items-center justify-center py-8 outline outline-2 rounded-md relative bg-secondAccent overflow-y-scroll mx-auto w-[40rem] h-80 px-2 md:px-6 shadow-base">
+          <div className="flex flex-col items-center justify-center py-8 outline outline-2 rounded-md relative bg-secondAccent overflow-y-scroll mx-auto w-[40rem] min-h-min px-2 md:px-6 shadow-base">
             <ContainerHeader
               options={headerOptions}
               handleClick={handleClick}
               isMenuOpen={isMenuOpen}
             />
-            <div className="gap-y-4">
+            <div className="mt-10 gap-y-4">
               {children}
             </div>
           </div>
